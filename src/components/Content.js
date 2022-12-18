4d23e00104ca891154fac20a3f807350c8d28ecf
hsariaslan <hakan.sariaslan.job@gmail.com>
2022-12-18 20:45:54 +0300

    Initial commit

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+
+function Content() {
+  return (
+    <main>
+      <Navbar/>
+      content
+    </main>
+  );
+}
+
+export default Content;
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <main>
+      navbar
+    </main>
+  );
+}
+
+export default Navbar;
